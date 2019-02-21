e71e4100c784007211276755894f637cd36aa92a
kco1ram3 <puvanate@hotmail.com>
2019-02-21 19:46:45 +0700

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -38,7 +38,7 @@ UserSchema.methods.toJSON = function () {
   var userObject = user.toObject();
 
   return _.pick(userObject, ['_id', 'email']);
-}
+};
 
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
@@ -50,7 +50,27 @@ UserSchema.methods.generateAuthToken = function () {
   return user.save().then(() => {
     return token;
   });
-}
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var user = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return user.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 
 var User = mongoose.model('User', UserSchema);
 
